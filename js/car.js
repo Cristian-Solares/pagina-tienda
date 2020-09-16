04e580c31b58052b00c5e4a840c2f68129a4e689
cistian solares <crizsolares2@gmail.com>
2020-09-16 00:24:33 -0500

    carrucel echo

---
+++ js/car.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  M.Carousel.init(elems, { duration: 150 });
+});
+
+// Or with jQuery
